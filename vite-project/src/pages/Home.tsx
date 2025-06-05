f1264eeb4f80cbe7ae3b4fc1fc2cdce148925807
nazmisani <nazminouval@gmail.com>
2025-06-06 02:32:31 +0700

    fix: update MovieCard background color and correct section title in Home page

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   useEffect(() => {
     async function fetchNowPlaying() {
       try {
-        const response = await tmdb.get("/movie/now_playing"); // v4
+        const response = await tmdb.get("/movie/now_playing");
         console.log(response.data);
-        setMovies(response.data.results); // tergantung struktur respon
+        setMovies(response.data.results);
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
@@ -22,7 +22,7 @@ export default function Home() {
     <>
       <div className="min-h-screen flex justify-center px-6 py-10 text-white">
         <div className="w-full max-w-7xl pl-10">
-          <h1 className="text-3xl font-bold mb-6">Your Watchlist</h1>
+          <h1 className="text-3xl font-bold mb-6">Now Playing</h1>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-12">
             {movies.map((movie, id) => (
               <MovieCard movie={movie} key={id} />
